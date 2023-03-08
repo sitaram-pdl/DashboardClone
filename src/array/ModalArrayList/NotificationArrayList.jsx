bd01bd23dc423fcb0ba4569690491ff7a55ccb0c
mrGannu <mrgithub69@gmail.com>
2023-03-08 15:39:25 +0545

    add notification modal

---
+++ src/array/ModalArrayList/NotificationArrayList.jsx
@@ -0,0 +1,40 @@
+const NotificationArrayList = [
+  {
+    icon: "document-text-outline",
+    name: "New files available",
+    time: "10 hour ago",
+    right_arr: "chevron-forward-outline",
+    bg: "#f10075",
+  },
+  {
+    icon: "diamond-outline",
+    name: "Updates Available ",
+    time: "2 days ago",
+    bg: "#4800c9",
+  },
+  {
+    icon: "water-outline",
+    name: "New Order Received ",
+    time: "1 hour ago",
+    bg: "#22c03c",
+  },
+  {
+    icon: "mail-outline",
+    name: "New review received ",
+    time: "1 day ago",
+    bg: "#fbbc0b",
+  },
+  {
+    icon: "people-outline",
+    name: "22 verified registrations ",
+    time: "2 hour ago",
+    bg: "#ee335e",
+  },
+  {
+    icon: "checkmark-circle-outline",
+    name: "Project has been approved ",
+    time: "4 hour ago",
+    bg: "#0162e8",
+  },
+];
+export default NotificationArrayList;
--- src/modals/Notifications/NotificationsModal.jsx
+++ src/modals/Notifications/NotificationsModal.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Notifications/notificationmodal.css";
+import NotificationArrayList from "../../array/ModalArrayList/NotificationArrayList";
 
 const NotificationsModal = ({ handleNotificationModal }) => {
   return (
@@ -13,7 +14,31 @@ const NotificationsModal = ({ handleNotificationModal }) => {
           </div>
           <p>You have 4 unread notifications</p>
         </div>
-        <div className="notification_body"></div>
+        <div className="notification_body">
+          {NotificationArrayList.map((dat) => {
+            return (
+              <>
+                <div className="individual_notification">
+                  <div className="individual_notification_div__left">
+                    <ion-icon
+                      name={dat.icon}
+                      style={{ background: `${dat.bg}` }}
+                    ></ion-icon>
+                  </div>
+                  <div className="individual_notification_div__right">
+                    <div className="row1">
+                      <h3>{dat.name}</h3>
+                      <small>{dat.time}</small>
+                    </div>
+                    <div className="row2">
+                      <ion-icon name={dat.right_arr}></ion-icon>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
         <div className="notification_footer">
           <button type="submit" className="modal_btn">
             view all
